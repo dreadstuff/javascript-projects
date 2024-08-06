019baf1c75dfcf01b58533f62e0e8d804644b705
dreadstuff <gavinkolasa@gmail.com>
2024-08-05 20:18:06 -0500

    commit 1 studio

--- dom-and-events/studio/scripts.js
+++ dom-and-events/studio/scripts.js
@@ -1,2 +1,27 @@
 // Write your JavaScript code here.
 // Remember to pay attention to page loading!
+window.addEventListener("load", function() {
+    console.log("all elements loaded");
+
+const takeOffButton = document.getElementById("takeoff");
+const flightStatusID = document.getElementById("flightStatus");
+const flightDisplayID = document.getElementById("flightDisplay");
+const shuttleBackgroundID = document.getElementById("shuttleBackground");
+const spaceShuttleHeightID = document.getElementById("spaceShuttleHeight");
+
+takeOffButton.addEventListener('click', function() {
+    let takeOffClick = window.confirm("Confirm that the shuttle is ready for takeoff.");
+    if (takeOffClick === true) {
+        flightStatusID.innerHTML = "Shuttle in flight.";
+        shuttleBackgroundID.style.background = "blue";
+        spaceShuttleHeightID.innerHTML = "10000";
+    }
+})
+
+///land button
+const landButtonID = document.getElementById("landing");
+landButtonID.addEventListener("click", function() {
+    window.alert("The shuttle is landing. Landing gear engaged");
+
+
+})})
